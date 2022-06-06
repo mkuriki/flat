f7c3afbb9ddc19ab09666bcd6bdd6380d47563b9
mkuriki <k.tenten0820@gmail.com>
2022-06-06 14:31:45 +0000

    [add]routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "f69da5244e644d4ea56a88344b366099.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,39 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
+  #管理者側
+  namespace :admins do
+    #会員情報
+    resources :users
+  end
+  
+  #ユーザー側
+  namespace :public do
+    #トップ
+    root to: 'homes#top'
+    get "home/about" => 'homes#about'
+    #ログインユーザー情報
+    resources :users , only: [:show, :edit, :update]
+    get "users/:id/quit" => 'customers#quit'
+    patch "users/:id/quit" => 'customers#withdraw'
+    #投稿
+    resources :posts
+    #いいね機能
+    resources :favorites
+    #コメント機能
+    resources :post_comments
+    #グループ機能
+    resources :groups
+  end
+  
+  #検索
+  get "search" => "searches#search"
+  
+  
+  
+  
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
