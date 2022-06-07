770473c36b935f27c50afd16c1a6812006244e3c
mkuriki <k.tenten0820@gmail.com>
2022-06-07 11:07:05 +0000

    [add]投稿フォーム、投稿一覧表示

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,5 +1,11 @@
 class Public::PostsController < ApplicationController
   def index
+    @post = Post.new
+    @posts = Post.all
+  end
+  
+  def create
+    @new_post = Post.new(post_params)
   end
 
   def show
@@ -12,5 +18,12 @@ class Public::PostsController < ApplicationController
   end
 
   def destroy
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_params
+    params.require(:post).permit(:post_image, :title, :body)
   end
 end
