a5110b7b0d1e56d2cc3229e71fcc952bf1118975
mkuriki <k.tenten0820@gmail.com>
2022-06-07 08:01:37 +0000

    [add]管理、会員、ゲストごとのヘッダ－

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  #サインアウト後の変遷先
+  #管理側の場合管理用ログイン、それ以外はユーザーログインへ
+  def after_sign_out_path_for(resource)
+     if resource == :admin
+      new_admin_session_path
+     else
+      new_user_session_path
+     end
+  end
 end
