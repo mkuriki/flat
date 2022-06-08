70211e35356958c972ec243b573f99a0984b6385
mkuriki <k.tenten0820@gmail.com>
2022-06-08 11:01:58 +0000

    [add]admin_投稿削除権限、会員詳細

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  
   def show
     @user = User.find(params[:id])
   end
