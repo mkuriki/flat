e69c561e632d70fc6d271311a50bd2c7da4f5d68
mkuriki <k.tenten0820@gmail.com>
2022-06-08 11:16:59 +0000

    [add]admin_会員編集

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,31 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
   
   def show
+    @user = User.find(params[:id])
   end
   
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user)
+      flash[:notice] = 'successed'
+    else
+      render :edit
+      flash[:alret] = 'failed'
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :is_deleted)
   end
 end
