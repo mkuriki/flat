8855cd4659c22fbf803972366c5b1b9164d83973
mkuriki <k.tenten0820@gmail.com>
2022-06-09 10:03:01 +0000

    [update]コメント機能_非同期化

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -4,7 +4,6 @@ class Public::PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = @post.id
     comment.save
-    redirect_to request.referer
   end
 
   def destroy
